fa0b4e3197f80973d1195e7d223d51aa5403b9ac
Nsuresh25 <nsuresh77@gmail.com>
2025-05-09 13:22:33 +0530

    Feature-branch

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_instance" "nginx_server" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
-  key_name                    = "sql" #"linux" # Replace with your key pair name
+  key_name                    = "linux" # Replace with your key pair name
 
   user_data = <<-EOF
               #!/bin/bash
